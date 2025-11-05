59ff1932468746714b964c8f979a591e445ccc08
mossafaei <mostafa.safaeipour1@gmail.com>
2025-11-05 14:16:40 +0330

    Feat: add google analytics

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,17 @@ import { ThemeProvider } from "@mui/material/styles";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { lightTheme } from "./config/theme";
+import { initGA } from "./analytics";
+import { useEffect } from "react";
 import Home from "./pages/home";
 
 function App() {
   let selectedTheme = lightTheme;
   const queryClient = new QueryClient();
+
+  useEffect(() => {
+    initGA();
+  }, []);
 
   return (
     <ThemeProvider theme={selectedTheme}>
